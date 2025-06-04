4d753a3d78a466f3bd45fb0dafe320abfad245b4
anderson7992 <andersongeo7992@gmail.com>
2025-06-03 21:59:01 -0300

    Create ZY - Exercicio-51.c

---
+++ IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/IFSC/ZY - Exercicio-51.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+51. Faça um algoritmo que leia dois números e mostre qual o maior dos dois .
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int v1, v2;
+    
+    printf("Insira 2 valores diferentes: ");
+    scanf("%d",&v1);
+    scanf("%d",&v2);
+    
+    printf("\n\nValor maior: ");
+    
+    if(v1 > v2)             
+        printf("%d ", v1);
+
+    else if (v2 > v1 )    
+        printf("%d ", v2);
+    
+    else
+        printf("%d = %d", v1, v2);
+
+return 0;
+}
